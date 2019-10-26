c696f75e9e612c99274b51fa6a5b5ac86d469661
Aliaksei Syrel <alex.syrel@gmail.com>
2019-10-26 17:34:27 +0200

    add be_zero to points and sizes

--- boxer/point.rs
+++ boxer/point.rs
@@ -5,9 +5,23 @@ pub struct BoxerPointU64 {
     pub y: u64,
 }
 
+impl BoxerPointU64 {
+    pub fn be_zero(&mut self) {
+        self.x = 0;
+        self.y = 0;
+    }
+}
+
 #[derive(Debug, Copy, Clone, Default)]
 #[repr(C)]
 pub struct BoxerPointF64 {
     pub x: f64,
     pub y: f64,
+}
+
+impl BoxerPointF64 {
+    pub fn be_zero(&mut self) {
+        self.x = 0.0;
+        self.y = 0.0;
+    }
 }
--- boxer/size.rs
+++ boxer/size.rs
@@ -5,9 +5,23 @@ pub struct BoxerSizeU64 {
     pub height: u64,
 }
 
+impl BoxerSizeU64 {
+    pub fn be_zero(&mut self) {
+        self.width = 0;
+        self.height = 0;
+    }
+}
+
 #[derive(Debug, Copy, Clone, Default)]
 #[repr(C)]
 pub struct BoxerSizeF64 {
     pub width: f64,
     pub height: f64,
+}
+
+impl BoxerSizeF64 {
+    pub fn be_zero(&mut self) {
+        self.width = 0.0;
+        self.height = 0.0;
+    }
 }
