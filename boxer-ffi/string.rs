9edd309ee29ecb668a2989067a1f0c6415f0ab4e
Aliaksei Syrel <alex.syrel@gmail.com>
2019-10-28 18:21:25 +0100

    add boxer_string_set

--- boxer-ffi/string.rs
+++ boxer-ffi/string.rs
@@ -13,6 +13,24 @@ pub fn boxer_string_drop(_ptr: *mut BoxerString) {
     CBox::drop(_ptr)
 }
 
+/// I mutate the string to be backed by the copy of a given char buffer in utf-8
+#[no_mangle]
+pub fn boxer_string_set(_string_ptr: *mut BoxerString, _data_utf8: *mut c_char, _length: usize) {
+    CBox::with_optional_raw(_string_ptr, |option| match option {
+        None => {},
+        Some(string) => {
+            if !_length == 0 {
+                return
+            }
+            let contents = BoxerString::chars_to_string(_data_utf8);
+            if contents.len() != _length {
+                eprintln!("[boxer_string_set] The actual size of the buffer ({:?}) differs from the given ({:?}) ", contents.len(), _length);
+            }
+            string.set_string(contents);
+        },
+    })
+}
+
 #[no_mangle]
 pub fn boxer_string_get_length(_string_ptr: *mut BoxerString) -> usize {
     CBox::with_optional_raw(_string_ptr, |option| match option {
