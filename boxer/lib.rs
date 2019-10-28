db8a062dc156dbc1f397aa69973d0e969e06052c
Aliaksei Syrel <alex.syrel@gmail.com>
2019-10-28 17:53:03 +0100

    make from_raw public but unsafe

--- boxer/lib.rs
+++ boxer/lib.rs
@@ -38,29 +38,24 @@ impl CBox {
         Box::into_raw(Box::new(object))
     }
 
-    /// This is dangerous! Rust takes the control over the memory back
-    fn from_raw<T>(pointer: *mut T) -> Box<T> {
-        assert_eq!(pointer.is_null(), false, "CBox::from_raw(): Pointer must not be null!");
-        unsafe { Box::from_raw(pointer) }
-    }
-
     /// Tell Rust to take back the control over memory
-    /// This is dangerous!
-    pub unsafe fn take_back<T>(pointer: *mut T) -> Box<T> {
-        Self::from_raw(pointer)
+    /// This is dangerous! Rust takes the control over the memory back
+    pub unsafe fn from_raw<T>(pointer: *mut T) -> Box<T> {
+        assert_eq!(pointer.is_null(), false, "CBox::from_raw(): Pointer must not be null!");
+        Box::from_raw(pointer)
     }
 
     pub fn drop<T>(pointer: *mut T) {
         if pointer.is_null() {
             return;
         }
-        CBox::from_raw(pointer);
+        unsafe { CBox::from_raw(pointer) };
     }
 
     pub fn with_raw<F, R, T>(pointer: *mut T, block: F) -> R where F : FnOnce(&mut Box<T>) -> R {
         assert_eq!(pointer.is_null(), false, "CBox::with_raw(): Pointer must not be null!");
 
-        let mut boxed_object: Box<T> = Self::from_raw(pointer);
+        let mut boxed_object: Box<T> = unsafe { Self::from_raw(pointer) };
         let result: R = block(&mut boxed_object);
         Self::into_raw(boxed_object);
         result
@@ -70,8 +65,8 @@ impl CBox {
         assert_eq!(pointer_1.is_null(), false, "CBox::with_raw(): Pointer #1 must not be null!");
         assert_eq!(pointer_2.is_null(), false, "CBox::with_raw(): Pointer #2 must not be null!");
 
-        let mut boxed_object_1: Box<T1> = Self::from_raw(pointer_1);
-        let mut boxed_object_2: Box<T2> = Self::from_raw(pointer_2);
+        let mut boxed_object_1: Box<T1> = unsafe { Self::from_raw(pointer_1) };
+        let mut boxed_object_2: Box<T2> = unsafe { Self::from_raw(pointer_2) };
         let result: R = block(&mut boxed_object_1, &mut boxed_object_2);
         Self::into_raw(boxed_object_1);
         Self::into_raw(boxed_object_2);
@@ -84,9 +79,9 @@ impl CBox {
         assert_eq!(pointer_2.is_null(), false, "CBox::with_raw(): Pointer #2 must not be null!");
         assert_eq!(pointer_3.is_null(), false, "CBox::with_raw(): Pointer #3 must not be null!");
 
-        let mut boxed_object_1: Box<T1> = Self::from_raw(pointer_1);
-        let mut boxed_object_2: Box<T2> = Self::from_raw(pointer_2);
-        let mut boxed_object_3: Box<T3> = Self::from_raw(pointer_3);
+        let mut boxed_object_1: Box<T1> = unsafe { Self::from_raw(pointer_1) };
+        let mut boxed_object_2: Box<T2> = unsafe { Self::from_raw(pointer_2) };
+        let mut boxed_object_3: Box<T3> = unsafe { Self::from_raw(pointer_3) };
         let result: R = block(&mut boxed_object_1, &mut boxed_object_2, &mut boxed_object_3);
         Self::into_raw(boxed_object_1);
         Self::into_raw(boxed_object_2);
@@ -95,7 +90,7 @@ impl CBox {
     }
 
     pub fn with_consumable_raw<F, T>(_ptr_consumable: *mut T, block: F) -> *mut T where F : FnOnce(T) -> T {
-        let consumable = *CBox::from_raw(_ptr_consumable);
+        let consumable = *unsafe { CBox::from_raw(_ptr_consumable) };
         let modified_consumable = block(consumable);
         CBox::into_raw(modified_consumable)
     }
