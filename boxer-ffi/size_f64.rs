01be4c35539365dce8134898a51958368a877cef
Aliaksei Syrel <alex.syrel@gmail.com>
2019-11-02 12:34:05 +0100

    size should use value box

--- boxer-ffi/size_f64.rs
+++ boxer-ffi/size_f64.rs
@@ -1,31 +1,32 @@
 use boxer::size::{BoxerSizeF64};
+use boxer::boxes::ValueBox;
 
 #[no_mangle]
-pub fn boxer_size_f64_create() -> *mut BoxerSizeF64 {
+pub fn boxer_size_f64_create() -> *mut ValueBox<BoxerSizeF64> {
     BoxerSizeF64::boxer_size_create()
 }
 
 #[no_mangle]
-pub fn boxer_size_f64_drop(_ptr: *mut BoxerSizeF64) {
+pub fn boxer_size_f64_drop(_ptr: *mut ValueBox<BoxerSizeF64>) {
     BoxerSizeF64::boxer_size_drop(_ptr);
 }
 
 #[no_mangle]
-pub fn boxer_size_f64_get_width(_ptr: *mut BoxerSizeF64) -> f64 {
+pub fn boxer_size_f64_get_width(_ptr: *mut ValueBox<BoxerSizeF64>) -> f64 {
     BoxerSizeF64::boxer_size_get_width(_ptr)
 }
 
 #[no_mangle]
-pub fn boxer_size_f64_set_width(_ptr: *mut BoxerSizeF64, width: f64) {
+pub fn boxer_size_f64_set_width(_ptr: *mut ValueBox<BoxerSizeF64>, width: f64) {
     BoxerSizeF64::boxer_size_set_width(_ptr, width);
 }
 
 #[no_mangle]
-pub fn boxer_size_f64_get_height(_ptr: *mut BoxerSizeF64) -> f64 {
+pub fn boxer_size_f64_get_height(_ptr: *mut ValueBox<BoxerSizeF64>) -> f64 {
     BoxerSizeF64::boxer_size_get_height(_ptr)
 }
 
 #[no_mangle]
-pub fn boxer_size_f64_set_height(_ptr: *mut BoxerSizeF64, height: f64) {
+pub fn boxer_size_f64_set_height(_ptr: *mut ValueBox<BoxerSizeF64>, height: f64) {
     BoxerSizeF64::boxer_size_set_height(_ptr, height);
 }
--- boxer-ffi/size_u64.rs
+++ boxer-ffi/size_u64.rs
@@ -1,31 +1,32 @@
 use boxer::size::{BoxerSizeU64};
+use boxer::boxes::ValueBox;
 
 #[no_mangle]
-pub fn boxer_size_u64_create() -> *mut BoxerSizeU64 {
+pub fn boxer_size_u64_create() -> *mut ValueBox<BoxerSizeU64> {
     BoxerSizeU64::boxer_size_create()
 }
 
 #[no_mangle]
-pub fn boxer_size_u64_drop(_ptr: *mut BoxerSizeU64) {
+pub fn boxer_size_u64_drop(_ptr: *mut ValueBox<BoxerSizeU64>) {
     BoxerSizeU64::boxer_size_drop(_ptr);
 }
 
 #[no_mangle]
-pub fn boxer_size_u64_get_width(_ptr: *mut BoxerSizeU64) -> u64 {
+pub fn boxer_size_u64_get_width(_ptr: *mut ValueBox<BoxerSizeU64>) -> u64 {
     BoxerSizeU64::boxer_size_get_width(_ptr)
 }
 
 #[no_mangle]
-pub fn boxer_size_u64_set_width(_ptr: *mut BoxerSizeU64, width: u64) {
+pub fn boxer_size_u64_set_width(_ptr: *mut ValueBox<BoxerSizeU64>, width: u64) {
     BoxerSizeU64::boxer_size_set_width(_ptr, width);
 }
 
 #[no_mangle]
-pub fn boxer_size_u64_get_height(_ptr: *mut BoxerSizeU64) -> u64 {
+pub fn boxer_size_u64_get_height(_ptr: *mut ValueBox<BoxerSizeU64>) -> u64 {
     BoxerSizeU64::boxer_size_get_height(_ptr)
 }
 
 #[no_mangle]
-pub fn boxer_size_u64_set_height(_ptr: *mut BoxerSizeU64, height: u64) {
+pub fn boxer_size_u64_set_height(_ptr: *mut ValueBox<BoxerSizeU64>, height: u64) {
     BoxerSizeU64::boxer_size_set_height(_ptr, height);
 }
