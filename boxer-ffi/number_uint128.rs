480d9fc502530e65b3d7571037a580a468ce3d15
Aliaksei Syrel <alex.syrel@gmail.com>
2019-11-02 13:09:22 +0100

    uint128 should be packed in a ValueBox

--- boxer-ffi/number_uint128.rs
+++ boxer-ffi/number_uint128.rs
@@ -1,60 +1,42 @@
-use boxer::CBox;
 use boxer::number::{BoxerUint128};
+use boxer::boxes::{ValueBox, ValueBoxPointer};
 
 #[no_mangle]
-pub fn boxer_number_uint128_create() -> *mut BoxerUint128 {
-    CBox::into_raw(BoxerUint128::default())
+pub fn boxer_number_uint128_create() -> *mut ValueBox<BoxerUint128> {
+    ValueBox::new(BoxerUint128::default()).into_raw()
 }
 
 #[no_mangle]
-pub fn glutin_number_uint128_drop(_ptr: *mut BoxerUint128) {
-    CBox::drop(_ptr)
+pub fn boxer_number_uint128_drop(_ptr: *mut ValueBox<BoxerUint128>) {
+    _ptr.drop();
 }
 
 #[no_mangle]
-pub fn boxer_number_uint128_get_low(_number_ptr: *mut BoxerUint128) -> u64 {
-    CBox::with_optional_raw(_number_ptr, |option| match option {
-        None => 0,
-        Some(number) => { number.low },
-    } )
+pub fn boxer_number_uint128_get_low(_number_ptr: *mut ValueBox<BoxerUint128>) -> u64 {
+    _number_ptr.with_not_null_return(0, | number | number.low)
 }
 
 #[no_mangle]
-pub fn boxer_number_uint128_set_low(_number_ptr: *mut BoxerUint128, low: u64) {
-    CBox::with_optional_raw(_number_ptr, |option| match option {
-        None => {},
-        Some(number) => { number.low = low },
-    } );
+pub fn boxer_number_uint128_set_low(_number_ptr: *mut ValueBox<BoxerUint128>, low: u64) {
+    _number_ptr.with_not_null(| number | number.low = low);
 }
 
 #[no_mangle]
-pub fn boxer_number_uint128_get_high(_number_ptr: *mut BoxerUint128) -> u64 {
-    CBox::with_optional_raw(_number_ptr, |option| match option {
-        None => 0,
-        Some(number) => { number.high },
-    } )
+pub fn boxer_number_uint128_get_high(_number_ptr: *mut ValueBox<BoxerUint128>) -> u64 {
+    _number_ptr.with_not_null_return(0, | number | number.high)
 }
 
 #[no_mangle]
-pub fn boxer_number_uint128_set_high(_number_ptr: *mut BoxerUint128, high: u64) {
-    CBox::with_optional_raw(_number_ptr, |option| match option {
-        None => {},
-        Some(number) => { number.high = high },
-    } );
+pub fn boxer_number_uint128_set_high(_number_ptr: *mut ValueBox<BoxerUint128>, high: u64) {
+    _number_ptr.with_not_null(| number | number.high = high);
 }
 
 #[no_mangle]
-pub fn boxer_number_uint128_set_max(_number_ptr: *mut BoxerUint128) {
-    CBox::with_optional_raw(_number_ptr, |option| match option {
-        None => {},
-        Some(number) => { number.set(std::u128::MAX) },
-    } );
+pub fn boxer_number_uint128_set_max(_number_ptr: *mut ValueBox<BoxerUint128>) {
+    _number_ptr.with_not_null(| number | number.set(std::u128::MAX));
 }
 
 #[no_mangle]
-pub fn boxer_number_uint128_set_min(_number_ptr: *mut BoxerUint128) {
-    CBox::with_optional_raw(_number_ptr, |option| match option {
-        None => {},
-        Some(number) => { number.set(std::u128::MIN) },
-    } );
+pub fn boxer_number_uint128_set_min(_number_ptr: *mut ValueBox<BoxerUint128>) {
+    _number_ptr.with_not_null(| number | number.set(std::u128::MIN));
 }
