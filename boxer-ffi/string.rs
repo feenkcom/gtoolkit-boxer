53afb862b0a42b6012ac2b819f499f97de1d7b0f
Aliaksei Syrel <alex.syrel@gmail.com>
2020-01-19 22:02:47 +0100

    add boxer_string_from_data (unowned)

--- boxer-ffi/string.rs
+++ boxer-ffi/string.rs
@@ -8,6 +8,10 @@ pub fn boxer_string_create() -> *mut BoxerString {
     CBox::into_raw(BoxerString::default())
 }
 
+pub fn boxer_string_from_data(data: *mut c_char, length: usize) -> *mut BoxerString {
+    CBox::into_raw(BoxerString::from_data(data, length))
+}
+
 #[no_mangle]
 pub fn boxer_string_drop(_ptr: *mut BoxerString) {
     CBox::drop(_ptr)
