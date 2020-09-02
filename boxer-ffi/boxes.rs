82ed04760d00b04387456406762b178859ebbfa4
Aliaksei Syrel <alex.syrel@gmail.com>
2020-09-02 14:03:21 +0200

    add boxer_value_box_is_valid

--- boxer-ffi/boxes.rs
+++ boxer-ffi/boxes.rs
@@ -5,3 +5,8 @@ use std::os::raw::c_void;
 pub fn boxer_value_box_get_pointer(ptr: *mut ValueBox<c_void>) -> *const c_void {
     ptr.get_ptr()
 }
+
+#[no_mangle]
+pub fn boxer_value_box_is_valid(ptr: *mut ValueBox<c_void>) -> bool {
+    ptr.is_valid()
+}
